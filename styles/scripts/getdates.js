ea5d6e1dcf16589b261105802507a00ed1ac0754
henryesteban89 <henryesteban89@gmail.com>
2026-09-05 19:35:15 -0600

    homepage

---
+++ styles/scripts/getdates.js
@@ -0,0 +1,6 @@
+// Output current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Output last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
